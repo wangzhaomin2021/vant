02795b316030037751e068a0d6d7486eea405511
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-16 15:37:12 +0800

    @wzm preview

--- packages/vant/src/count-down/CountDown.tsx
+++ packages/vant/src/count-down/CountDown.tsx
@@ -1,3 +1,4 @@
+// @wzm preview 2022-12-16
 import { watch, computed, defineComponent, type ExtractPropTypes } from 'vue';
 
 // Utils
@@ -39,24 +40,28 @@ export default defineComponent({
       onFinish: () => emit('finish'),
     });
 
+    // 倒计时显示文本
     const timeText = computed(() => parseFormat(props.format, current.value));
 
+    // 重置时间
     const resetTime = () => {
       reset(+props.time);
 
-      if (props.autoStart) {
+      if (props.autoStart) { // 自动倒计时
         start();
       }
     };
 
     watch(() => props.time, resetTime, { immediate: true });
 
+    // 对外暴露
     useExpose({
       start,
       pause,
       reset: resetTime,
     });
 
+    // template
     return () => (
       <div role="timer" class={bem()}>
         {slots.default ? slots.default(current.value) : timeText.value}
